fff662484e747ae41c7d0f6f47d152eedb157859
ItsJonQ <itsjonq@gmail.com>
2016-06-23 18:20:50 -0400

    Adds sass-pathfinder

--- scripts/gulp/pipes/sass.js
+++ scripts/gulp/pipes/sass.js
@@ -3,21 +3,24 @@
 
 var gulp = require('gulp');
 var lazypipe = require('lazypipe');
+var pathfinder = require('sass-pathfinder');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 var bourbon = require('bourbon').includePaths;
 var seedGrid = require('seed-grid');
 
+var includePaths = pathfinder(
+  global.config.src,
+  bourbon,
+  seedGrid
+);
+
 var pipe = lazypipe()
   .pipe(sourcemaps.init)
   .pipe(function() {
     return sass({
-      includePaths: [
-        global.config.src,
-        bourbon,
-        seedGrid
-      ]
+      includePaths: includePaths
     })
     .on('error', sass.logError);
   })
