c0b59ece883144bb01cbdf4878b8e0814b26c5fc
ItsJonQ <itsjonq@gmail.com>
2016-06-22 21:13:30 -0400

    Abstracts grid system to seed-grid pack

--- scripts/gulp/pipes/sass.js
+++ scripts/gulp/pipes/sass.js
@@ -6,12 +6,17 @@ var lazypipe = require('lazypipe');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
+var bourbon = require('bourbon').includePaths;
+var seedGrid = require('seed-grid');
+
 var pipe = lazypipe()
   .pipe(sourcemaps.init)
   .pipe(function() {
     return sass({
       includePaths: [
-        global.config.src
+        global.config.src,
+        bourbon,
+        seedGrid
       ]
     })
     .on('error', sass.logError);
