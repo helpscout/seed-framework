e0e6a778848560736e97950d408b0e5a31592726
ItsJonQ <itsjonq@gmail.com>
2016-06-29 13:37:18 -0400

    Updates scss-lint to sass-lint
    * updates/fixes watch script

--- scripts/gulp/base/lint/default.js
+++ scripts/gulp/base/lint/default.js
@@ -2,46 +2,14 @@
 'use strict';
 
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var lint = require('gulp-scss-lint');
-
-var exec = require('child_process').exec;
-var parse = function(data) {
-  return data.toString().replace(/\n\s*\n/g,'');
-};
-
-var commandOptions = {
-  env: process.env,
-  cwd: process.cwd(),
-  maxBuffer: 300 * 1024,
-};
-
-var lintProcess = function() {
-  gutil.log(gutil.colors.yellow('Testing your .scss files…'));
-  return new Promise(function(resolve, reject) {
-    exec('bundle exec scss-lint', commandOptions, function(error, report) {
-      if (report) {
-        return reject(report);
-      }
-      // Check for important error codes
-      if(error && error.code !== 1 && error.code !== 2 && error.code !== 65) {
-        return reject(report);
-      }
-      return resolve(report);
-    });
-  });
-};
+var sassLint = require('gulp-sass-lint');
 
 gulp.task('lint', function(callback) {
-  return lintProcess()
-    .then(function(report) {
-      var message = gutil.colors.green('Passed! Your .scss looks spectacular');
-      gutil.log(message, '😄');
-    })
-    .catch(function(report) {
-      var message = gutil.colors.red('Failed! There were errors in your .scss');
-      gutil.log(message, '😱');
-      console.log(parse(report));
-      process.exit(1);
-    });
+  return gulp.src(global.config.src + '/seed.scss')
+    .pipe(sassLint({
+      configFile: global.path + '/.sass-lint.yml'
+    }))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .on('close', callback);
 });
